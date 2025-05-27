f271bca9b1240fdd584916f7c4a20a42c1bed379
Callacos <callacos74@gmail.com>
2025-05-27 10:41:10 +0200

    fix: add missing newlines at the end of several files

--- TypeScript/task_0/js/main.ts
+++ TypeScript/task_0/js/main.ts
@@ -4,4 +4,44 @@ interface Student {
     lastName: string
     age: number
     location: string
-}
+};
+const student1: Student = {
+	firstName: "John",
+	lastName: "Doe",
+	age: 20,
+	location: "New York"
+};
+const student2: Student = {
+	firstName: "Jane",
+	lastName: "Smith",
+	age: 22,
+	location: "Los Angeles"
+};
+const studentsList: Student[] = [student1, student2];
+
+const table = document.createElement("table");
+
+
+const header = document.createElement("tr");
+["First Name", "Location"].forEach((text) => {
+    const th = document.createElement("th");
+    th.textContent = text;
+    header.appendChild(th);
+});
+table.appendChild(header);
+
+studentsList.forEach((student) => {
+    const row = document.createElement("tr");
+
+    const nameCell = document.createElement("td");
+    nameCell.textContent = student.firstName;
+
+    const locationCell = document.createElement("td");
+    locationCell.textContent = student.location;
+
+    row.appendChild(nameCell);
+    row.appendChild(locationCell);
+    table.appendChild(row);
+});
+
+document.body.appendChild(table);
